bd1c348aa3636b5b5752ae5100db0672f95cb9c0
Guillermo Loaysa <gloaysa@gmail.com>
2024-01-21 15:15:59 +0100

    Initial Commit

---
+++ local-todos/local_todosApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_todosApp.swift
+//  local-todos
+//
+//  Created by Guillermo Loaysa on 21/1/24.
+//
+
+import SwiftUI
+
+@main
+struct local_todosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ local-todosUITests/local_todosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  local_todosUITestsLaunchTests.swift
+//  local-todosUITests
+//
+//  Created by Guillermo Loaysa on 21/1/24.
+//
+
+import XCTest
+
+final class local_todosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
